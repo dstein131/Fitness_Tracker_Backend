173a9f46b9aac3da06c31672cdd0ff255bbe33b1
David Stein <dstein131@gmail.com>
2022-07-07 18:46:02 -0400

    ok

--- db/client.js
+++ db/client.js
@@ -2,6 +2,6 @@
 
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/fitness-dev");
+const client = new Client(process.env.DATABASE_URL || "postgres://localhost:5432/fitness-dev");
 
 module.exports =  client
